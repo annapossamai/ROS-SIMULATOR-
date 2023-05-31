64666f6d1216e51c0da6c2796803b915eca264d0
annapossamai <114078218+annapossamai@users.noreply.github.com>
2023-05-31 15:32:53 +0000

    Update dist_vel_C.py

--- Assignment2_2022_ResearchTrack/scripts/dist_vel_C.py
+++ Assignment2_2022_ResearchTrack/scripts/dist_vel_C.py
@@ -1,5 +1,25 @@
 #! /usr/bin/env python
 
+"""
+.. module:: dist_vel_C
+	:platform: Unix
+	:synopsis: Python module for the subscriber node C.
+
+.. moduleauthor:: Anna Possamai
+
+This node implements a *subscriber node* that subscribes *to the robots position and velocity* (using the custom message) 
+and prints on the console the distance of the robot from the target (set in node action_client_A), 
+and its average speed with the frequency set as a parameter in the launch file.
+
+Subscriber:
+	/pos_vel
+
+Service client:
+	/set_frequency
+
+"""
+
+
 # import ros stuff
 import rospy
 from assignment_2_2022.msg import Pos_vel
@@ -15,60 +35,73 @@ tp=0
 
 def callback(msg):
 
-    global f, tp
+	"""
+	This callback function subscribes to the '/pos_vel' topic and prints the distance of the robot from the target and its average speed.
 
-    # convert the current time in milliseconds
-    c_time= time.time()*1000
+	Args:
+		msg(Pos_vel): the position and velocity of the robot
 
-    # compute the period in milliseconds
-    p = (1/f)*1000
+	"""
+
+	global f, tp
+
+	# convert the current time in milliseconds
+	c_time= time.time()*1000
+
+	# compute the period in milliseconds
+	p = (1/f)*1000
 
 
-    # calculate distance and the average speed according to the calculated period
-    if c_time - tp > p:
+# calculate distance and the average speed according to the calculated period
+	if c_time - tp > p:
  
-        # Get the desired position
-        x_desired = rospy.get_param("des_pos_x")
-        y_desired = rospy.get_param("des_pos_y")
+		# Get the desired position
+		x_desired = rospy.get_param("des_pos_x")
+		y_desired = rospy.get_param("des_pos_y")
 
-        #Get the actual position and speed
-        x = msg.x
-        y = msg.y
-        v_x=msg.vel_x
-        v_y=msg.vel_y
+		#Get the actual position and speed
+		x = msg.x
+		y = msg.y
+		v_x=msg.vel_x
+		v_y=msg.vel_y
 
-        # Compute the distance of the robot from the target
-        d = math.dist([x_desired,y_desired], [x,y])
+		# Compute the distance of the robot from the target
+		d = math.dist([x_desired,y_desired], [x,y])
 
-        # Compute the robot average speed
-        average_vel= math.sqrt(v_x**2 + v_y**2)
+		# Compute the robot average speed
+		average_vel= math.sqrt(v_x**2 + v_y**2)
 
-        #Print the distance and the average speed
-        print("Distance from the target: ", d)
-        print("Average speed: ", average_vel)
+		#Print the distance and the average speed
+		print("Distance from the target: ", d)
+		print("Average speed: ", average_vel)
 
-        # Update the last time the info was printed with the current time
-        tp = c_time
+		# Update the last time the info was printed with the current time
+		tp = c_time
 
 
 def main():
 
-    #Global variable
-    global f
+	"""
+	This function initializes the node and subscribes to the '/pos_vel' topic.
+  
+	"""
 
-    #Initialize the node
-    rospy.init_node('dist_vel')
+	#Global variable
+	global f
 
-    # Get the publish frequency parameter
-    f = rospy.get_param("/publish_frequency")
+	#Initialize the node
+	rospy.init_node('dist_vel')
 
-    # Subscriber to the topic to get the position and velocity
-    sub = rospy.Subscriber('/pos_vel', Pos_vel, callback)
+	# Get the publish frequency parameter
+	f = rospy.get_param("/publish_frequency")
 
-    #keep python from exiting until this node is stopped
-    rospy.spin()
+	# Subscriber to the topic to get the position and velocity
+	sub = rospy.Subscriber('/pos_vel', Pos_vel, callback)
+
+	#keep python from exiting until this node is stopped
+	rospy.spin()
 
 if __name__ == "__main__":
-    
-    main()
+   
+	main()
 
